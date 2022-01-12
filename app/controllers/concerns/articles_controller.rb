7eea9f5574a1085b7aa79ccdddcbf9c0512b889d
Chleont <chleontidis@yahoo.com>
2022-01-12 22:43:28 +0200

    Fix display error

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -5,4 +5,15 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 end
