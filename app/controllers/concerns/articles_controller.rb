f103c77e1021e44eec63c1c52463a4fa2f20cb89
Chleont <chleontidis@yahoo.com>
2022-01-27 12:21:28 +0200

    Add one to many

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -10,6 +10,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created succesfully."
       redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_093159) do
+ActiveRecord::Schema.define(version: 2022_01_27_100758) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6
     t.datetime "updated_at", precision: 6
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
